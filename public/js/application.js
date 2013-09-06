9ca212c27bab7722b627f2092e4c1b1a88587873
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 18:39:46 -0500

    .

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+   $('#new_q').on('click', function(event){
+      event.preventDefault();
+      $('<div class="question_white"><input class="centerwrap" placeholder="Enter Question" type="text" name="survey[:prompt]" /></div><hr>').appendTo("form#survey_form");
+   });
 });
