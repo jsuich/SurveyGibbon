4335c2d05343a3e5739c3d92ef50159fa3bfbcb8
Josh Suich <jsuich@gmail.com>
2013-09-06 15:00:11 -0500

    rebasing to master

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :surveys
 
-  validates :name, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }
+  
   validates :entered_password, :length => { :minimum => 6 }
   validates :email, :uniqueness => true, :format => /.+@.+\..+/ # imperfect, but okay
 
