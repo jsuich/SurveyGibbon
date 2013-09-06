1422e7b618e18e49b87f1ca25fdd0cfce3643192
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 16:37:42 -0500

    jquery append form elements

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+   $('#new_q').on('click' function(event){
+      $('form#survey_form').append("<input type="text" name="survey[:prompt]">");
+   });
 });
