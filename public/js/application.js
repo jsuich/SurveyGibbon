23d1fd3e10db3d78c453cc67dcb35d1f9fd0ee82
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 18:18:23 -0500

    survey append and styles

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
-   $('#new_q').on('click' function(event){
-      $('form#survey_form').append("<input type="text" name="survey[:prompt]">");
+   $('#new_q').on('click', function(event){
+      event.preventDefault();
+      $('<div class="question_white"><input class="centerwrap" placeholder="Enter Question" type="text" name="survey[:prompt]" /></div><hr>').appendTo("form#survey_form");
    });
 });
