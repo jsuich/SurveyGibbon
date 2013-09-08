759a326875b7135445854e462f255c495e254e25
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-08 00:00:12 -0500

    radio buttons

--- public/js/application.js
+++ public/js/application.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
   $("#sortable").sortable({
     stop: function(ui, event){
       event.item.children(1).attr('name',"survey[prompts][" + n + "]");
+      event.item.children(1).css('margin', '0 0px');
+      event.item.children(1).css('width', '100%');
+      event.item.css('border', '1px solid rgba(5,130,85,0.7)');
+      event.item.css('margin-bottom', '30px');
+      event.item.children(1).css('background', 'rgba(251,251,251,1)');
+      event.item.css('width','100%');
       n++;
     }
   });
@@ -14,6 +20,15 @@ $(document).ready(function() {
     helper: "clone",
     revert: "invalid",
   });
+
+  $("#draggable_2").draggable({
+    stop: function(ui,event) {
+      event.item.append('<a id="gen_radio" href="#">Add Choice</a>');
+   } 
+  });
+  
+
+      
   
   $( "ul, li" ).disableSelection();
 
